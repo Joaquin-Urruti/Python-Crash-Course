7e2fdb07ca36e67950c3ee0d4ef1514d01999963
Joaquín Urruti <urrutijoaquin@gmail.com>
2023-12-10 10:36:09 -0300

    Chapter 10, ex 10-8

---
+++ Chapter 10/ch10_p226_108_perros_y_gatos.py
@@ -0,0 +1,15 @@
+def read_files(file_list):
+	for file_name in file_list:
+		try:
+			with open(file_name, 'r') as file_object:
+				content = file_object.read()
+
+		except FileNotFoundError:
+			print(f'{file_name} not found')
+
+		else:
+			print(f'\n{file_name}:\n{content}')
+
+file_list = ['gatos.txt', 'perros.txt']
+
+read_files(file_list)
