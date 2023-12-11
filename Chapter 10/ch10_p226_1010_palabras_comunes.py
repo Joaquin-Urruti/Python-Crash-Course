918a14befcd5e0fb82709a18d4eee37f65eb1b6b
Joaquín Urruti <urrutijoaquin@gmail.com>
2023-12-10 22:49:19 -0300

    Chapter 10, p226 ex 10-10

--- Chapter 10/ch10_p226_1010_palabras_comunes.py
+++ Chapter 10/ch10_p226_1010_palabras_comunes.py
@@ -2,9 +2,9 @@ def contar_palabras(file_name, word):
 	with open(file_name, 'r', encoding='UTF-8') as file_object:
 		content = file_object.read()
 	cuenta = content.lower().count(word)
-	print(f'The word "{word}" appears {cuenta} in {file_name}.')
+	print(f'The word "{word}" appears {cuenta} times in {file_name}.')
 
 files_list = ['hamlet.txt','iliada.txt','platon.txt']
 
 for file_name in files_list:
-	contar_palabras(file_name, 'tit')
+	contar_palabras(file_name, 'the')
