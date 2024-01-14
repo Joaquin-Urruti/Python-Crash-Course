442f4f05bc937d600eb6afbad9bf1621a8963dc8
Joaquín Urruti <urrutijoaquin@gmail.com>
2024-01-14 15:48:33 -0300

    Project 2, chapter 15, p356, ex153

--- Project 2/Chapter 15/ch15_p356_153_movimiento_molecular.py
+++ Project 2/Chapter 15/ch15_p356_153_movimiento_molecular.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-	rw = RandomWalk(num_points=100_000)
+	rw = RandomWalk(num_points=5_000)
 
 	rw.fill_walk()
 
 	plt.style.use('classic')
-	fig, ax = plt.subplots(figsize=(15, 9), dpi=100)
+	fig, ax = plt.subplots(figsize=(15, 9))
 	point_numbers = range(rw.num_points)
-	ax.scatter(rw.x_values, rw.y_values, c=point_numbers , cmap=plt.cm.Blues, s=0.5, edgecolors='none')
+	ax.plot(rw.x_values, rw.y_values, linewidth=0.5)
+	ax.set_aspect('equal')
+
 	ax.scatter(0, 0, c='green', s=100, edgecolors='none')
 	ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', s=100, edgecolors='none')
 	ax.get_xaxis().set_visible(False)
