cfe011b84f0c07c7f0a6a518e61b271c8f52d8e0
Joaquín Urruti <urrutijoaquin@gmail.com>
2024-02-09 14:11:36 -0300

    Project 2, chapter 16, p391, ex168

--- Project 2/Chapter 16/ch16_p391_168_terr_recientes.py
+++ Project 2/Chapter 16/ch16_p391_168_terr_recientes.py
@@ -4,7 +4,7 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Explorar el archivo
-filename = 'my_readable_file.json'
+filename = 'eq_data_30_day_m1.geojson'
 with open(filename) as f:
 	all_eq_data = json.load(f)
 
@@ -12,14 +12,10 @@ all_eq_dicts = all_eq_data['features']
 
 mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-	mag = eq_dict['properties']['mag']
-	lon = eq_dict['geometry']['coordinates'][0]
-	lat = eq_dict['geometry']['coordinates'][1]
-	title = eq_dict['properties']['title']
-	mags.append(mag)
-	lons.append(lon)
-	lats.append(lat)
-	hover_texts.append(title)
+	mags.append(eq_dict['properties']['mag'])
+	lons.append(eq_dict['geometry']['coordinates'][0])
+	lats.append(eq_dict['geometry']['coordinates'][1])
+	hover_texts.append(eq_dict['properties']['title'])
 
 
 # Mapear los terremotos
@@ -37,7 +33,9 @@ data = [{
 },
 }]
 
-my_layout = Layout(title='Global Earthquakes')
+title = all_eq_data['metadata']['title']
+
+my_layout = Layout(title=f'{title}')
 
 fig = {'data':data, 'layout':my_layout}
 offline.plot(fig, filename='global_earthquakes.html')
